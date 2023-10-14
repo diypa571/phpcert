284135a20f55e032a00ba569d0e61cbd46123978
Diyar Parwana <diypa571@gmail.com>
2023-10-14 21:02:00 +0200

    Update crack.php

--- crack.php
+++ crack.php
@@ -10,78 +10,63 @@
  <body>
 
 <h1>Diyar Parwana</h1>
-
 <p> Building Web Applications in PHP <em> University of Michigan </em></p>
-<p> This application takes an MD5 hash of a four digit pin and check all about 10000 possible four digit PINs to determine the PIN.Using md5 for hashing with md5 and reversing  </p>
-222 
+<p> This application takes an MD5 hash of a four digit pin and check all about 10000 possible four digit PINs to determine the PIN.
+  </p>
+<p>Try enter this hash <br>81dc9bdb52d04dc20036dbd8313ed055 </p>
 <?php
-  // Diyar Parwana
-  // University of Michigan
+   // Diyar Parwana
+  // PHP University of Michigan
   $startTime = microtime(true);
   $isFound= false;
   $hashedInput = "";
 
-    if(isset($_GET['md5']) && !empty($_GET['md5'])) {
-    $hashedInput = (string)$_GET['md5'];
-    echo    $hashedInput = md5($inputValue);
-    echo " <br>";
-    echo    $hashedInput = md5((int)$_GET['md5']);
-    echo "<hr>";
-    }
-
-
+  if(isset($_GET['md5']) && !empty($_GET['md5'])) {
+  $input = $_GET['md5'];
+  $hashedInput = (string)$_GET['md5'];
+   echo "<hr>";
+  }
   echo "<h3> Debug output </h3>";
   $startTime = microtime(true);
-
   // Creating a loop
   for ($x = 0; $x <= 9999; $x++) {
-
-
     // For the 4 digits
-  /*  while (strlen($x) < 4) {
-        $x = '0'.$x;
-    }
-    */
-    // A better alternative instead of my while loop is str_pad
-     $x=str_pad($x, 4, '0', STR_PAD_LEFT);
+   while (strlen($x) < 4) {
 
-    // Diysplay the first 15 rows
-    $md5hash=hash('md5',$x);
-    if($x<=10000){
-    echo "$md5hash $x<br>";
+        $x =  '0'.$x;
     }
+    // Diysplay the first 15 rows
+
+
+      $md5hash = hash('md5', $x);
+      if ($x <= 15) {  // Corrected the syntax here
+          echo "$md5hash   $x";
+          echo "<br>";
+      }
+
 
     if ($md5hash == $_GET["md5"]){
-        $isFound = true;
-        $timeElapsed = microtime(true) - $startTime;
-        echo "<br> Total checks: $x <br>";
-        echo "Ellapsed time: $timeElapsed <br> ";
-        echo "PIN is: $x <br>";
-        break;
+    $isFound = true;
+    $timeElapsed = microtime(true) - $startTime;
+    echo "<br> Total checks: $x <br>";
+    echo "Ellapsed time: $timeElapsed <br> ";
+    echo "PIN is: $x <br>";
+    break;
     }
    }
 
 
-
-
-    // Calculation for the Elapsed time
-   $timeElapsed = microtime(true) - $startTime;
-    echo " <br> Elapsed time:" . $timeElapsed;
-
-   if ($isFound) {
+   if (!$isFound) {
+      $timeElapsed = microtime(true) - $startTime;
        echo "<br> PIN: Not  Found <br>";
-   }
-
-   else {
-     echo "<br> PIN:  Found " . $md5hash ." <br>";
    }
 
 ?>
 
- <form action="index.php" method="get">
+<form action="index.php" method="get">
 <input type="text" name="md5" size="40" value="<?= htmlentities($md5) ?>">
 <input type="submit" value="Crack MD5"/>
- </form>
+</form>
 
  </body>
  </html>
